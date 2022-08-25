268043e425b9d0bb62884accc4bd5843138ee778
Romaric BELTRAN <romaric.beltran@rnd-learning.com>
2022-08-25 19:30:17 +0200

    Add lb theme

---
+++ theme/lb/settings.php
@@ -0,0 +1,111 @@
+<?php
+
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();                                                                                                
+                                                                                                                                    
+// This is used for performance, we don't need to know about these settings on every page in Moodle, only when                      
+// we are looking at the admin settings pages.                                                                                      
+if ($ADMIN->fulltree) {                                                                                                             
+                                                                                                                                    
+    // Boost provides a nice setting page which splits settings onto separate tabs. We want to use it here.                         
+    $settings = new theme_boost_admin_settingspage_tabs('themesettinglb', get_string('configtitle', 'theme_lb'));             
+                                                                                                                                    
+    // Each page is a tab - the first is the "General" tab.                                                                         
+    $page = new admin_settingpage('theme_lb_general', get_string('generalsettings', 'theme_lb'));                             
+
+    // Unaddable blocks.
+    // Blocks to be excluded when this theme is enabled in the "Add a block" list: Administration, Navigation, Courses and
+    // Section links.
+    $default = 'navigation,settings,course_list,section_links';
+    $setting = new admin_setting_configtext('theme_lb/unaddableblocks',
+        get_string('unaddableblocks', 'theme_lb'), get_string('unaddableblocks_desc', 'theme_lb'), $default, PARAM_TEXT);
+    $page->add($setting);
+
+    // Logo file setting.
+    $name = 'theme_lb/logo';
+    $title = get_string('logo', 'theme_lb');
+    $description = get_string('logodesc', 'theme_lb');
+    $opts = array('accepted_types' => array('.png', '.jpg', '.gif', '.webp', '.tiff', '.svg'), 'maxfiles' => 1);
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'logo', 0, $opts);
+    $page->add($setting);
+
+    // Favicon setting.
+    $name = 'theme_lb/favicon';
+    $title = get_string('favicon', 'theme_lb');
+    $description = get_string('favicondesc', 'theme_lb');
+    $opts = array('accepted_types' => array('.ico'), 'maxfiles' => 1);
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'favicon', 0, $opts);
+    $page->add($setting);
+
+    // Replicate the preset setting from boost.                                                                                     
+    $name = 'theme_lb/preset';                                                                                                   
+    $title = get_string('preset', 'theme_lb');                                                                                   
+    $description = get_string('preset_desc', 'theme_lb');                                                                        
+    $default = 'default.scss';                                                                                                      
+                                                                                                                                    
+    // We list files in our own file area to add to the drop down. We will provide our own function to                              
+    // load all the presets from the correct paths.                                                                                 
+    $context = context_system::instance();                                                                                          
+    $fs = get_file_storage();                                                                                                       
+    $files = $fs->get_area_files($context->id, 'theme_lb', 'preset', 0, 'itemid, filepath, filename', false);                    
+                                                                                                                                    
+    $choices = [];                                                                                                                  
+    foreach ($files as $file) {                                                                                                     
+        $choices[$file->get_filename()] = $file->get_filename();                                                                    
+    }                                                                                                                               
+    // These are the built in presets from Boost.                                                                                   
+    $choices['default.scss'] = 'default.scss';                                                                                      
+    $choices['plain.scss'] = 'plain.scss';                                                                                          
+                                                                                                                                    
+    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);                                     
+    $setting->set_updatedcallback('theme_reset_all_caches');                                                                        
+    $page->add($setting);                                                                                                           
+                                                                                                                                    
+    // Preset files setting.                                                                                                        
+    $name = 'theme_lb/presetfiles';                                                                                              
+    $title = get_string('presetfiles','theme_lb');                                                                               
+    $description = get_string('presetfiles_desc', 'theme_lb');                                                                   
+                                                                                                                                    
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,                                         
+        array('maxfiles' => 20, 'accepted_types' => array('.scss')));                                                               
+    $page->add($setting);     
+
+    // Variable $brand-color.                                                                                                       
+    // We use an empty default value because the default colour should come from the preset.                                        
+    $name = 'theme_lb/brandcolor';                                                                                               
+    $title = get_string('brandcolor', 'theme_lb');                                                                               
+    $description = get_string('brandcolor_desc', 'theme_lb');                                                                    
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, '');                                               
+    $setting->set_updatedcallback('theme_reset_all_caches');                                                                        
+    $page->add($setting);                                                                                                           
+
+    // Must add the page after definiting all the settings!                                                                         
+    $settings->add($page);                                                                                                          
+                                                                                                                                    
+    // Advanced settings.                                                                                                           
+    $page = new admin_settingpage('theme_lb_advanced', get_string('advancedsettings', 'theme_lb'));                           
+                                                                                                                                    
+    // Raw SCSS to include before the content.                                                                                      
+    $setting = new admin_setting_configtextarea('theme_lb/scsspre',                                                              
+        get_string('rawscsspre', 'theme_lb'), get_string('rawscsspre_desc', 'theme_lb'), '', PARAM_RAW);                      
+    $setting->set_updatedcallback('theme_reset_all_caches');                                                                        
+    $page->add($setting);                                                                                                           
+                                                                                                                                    
+    // Raw SCSS to include after the content.                                                                                       
+    $setting = new admin_setting_configtextarea('theme_lb/scss', get_string('rawscss', 'theme_lb'),                           
+        get_string('rawscss_desc', 'theme_lb'), '', PARAM_RAW);                                                                  
+    $setting->set_updatedcallback('theme_reset_all_caches');                                                                        
+    $page->add($setting);                                                                                                           
+                                                                                                                                    
+    // Google analytics block.
+    $name = 'theme_lb/googleanalytics';
+    $title = get_string('googleanalytics', 'theme_lb');
+    $description = get_string('googleanalyticsdesc', 'theme_lb');
+    $setting = new admin_setting_configtext($name, $title, $description, '');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    $settings->add($page);                                                                                                         
+}
