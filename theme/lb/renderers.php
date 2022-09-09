4b3958c5471ff2bb9b77e01fbb8eaf3329754f53
root <root@LAPTOP-963TMG9H.localdomain>
2022-09-09 19:02:20 +0200

    Fix some styles and add plugin attendance

--- theme/lb/renderers.php
+++ theme/lb/renderers.php
@@ -1,7 +1,27 @@
 <?php
 
-require_once($CFG->dirroot .'/course/renderer.php');
+class theme_lb_core_renderer extends theme_boost\output\core_renderer {
 
-class theme_lb_core_course_renderer extends core_course_renderer {
-    
+    /**
+     * Renders the login form.
+     *
+     * @param \core_auth\output\login $form The renderable.
+     * @return string
+     */
+    public function render_login(\core_auth\output\login $form) {
+        global $CFG, $SITE;
+
+        $context = $form->export_for_template($this);
+
+        $context->errorformatted = $this->error_text($context->error);
+        $url = $this->get_logo_url();
+        if ($url) {
+            $url = $url->out(false);
+        }
+        $context->logourl = $url;
+        $context->sitename = format_string($SITE->fullname, true,
+                ['context' => context_course::instance(SITEID), "escape" => false]);
+
+        return $this->render_from_template('theme_lb/loginform', $context);
+    }
 }
