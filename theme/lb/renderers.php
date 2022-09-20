ddd631ff112f734330d8767909359f6643ebc58d
Romaric BELTRAN <romaric.beltran@rnd-learning.com>
2022-09-20 17:48:46 +0200

    Store default "div" for a coursename in category_tree

--- theme/lb/renderers.php
+++ theme/lb/renderers.php
@@ -1,5 +1,8 @@
 <?php
 
+defined('MOODLE_INTERNAL') || die();
+require_once($CFG->dirroot . "/course/renderer.php");
+
 class theme_lb_core_renderer extends theme_boost\output\core_renderer {
 
     /**
@@ -65,5 +68,39 @@ class theme_lb_core_renderer extends theme_boost\output\core_renderer {
         }
 
         return $this->render_from_template('theme_lb/block', $context);
+    }
+}
+
+class theme_lb_core_course_renderer extends core_course_renderer {
+
+        /**
+     * Returns HTML to display course name.
+     *
+     * @param coursecat_helper $chelper
+     * @param core_course_list_element $course
+     * @return string
+     */
+    protected function course_name(coursecat_helper $chelper, core_course_list_element $course): string {
+        $content = '';
+        $nametag = 'div';
+        
+        $coursename = $chelper->get_course_formatted_name($course);
+        $coursenamelink = html_writer::link(new moodle_url('/course/view.php', ['id' => $course->id]),
+            $coursename, ['class' => $course->visible ? 'aalink' : 'aalink dimmed']);
+        $content .= html_writer::tag($nametag, $coursenamelink, ['class' => 'coursename']);
+        // If we display course in collapsed form but the course has summary or course contacts, display the link to the info page.
+        $content .= html_writer::start_tag('div', ['class' => 'moreinfo']);
+        if ($chelper->get_show_courses() < self::COURSECAT_SHOW_COURSES_EXPANDED) {
+            if ($course->has_summary() || $course->has_course_contacts() || $course->has_course_overviewfiles()
+                || $course->has_custom_fields()) {
+                $url = new moodle_url('/course/info.php', ['id' => $course->id]);
+                $image = $this->output->pix_icon('i/info', $this->strings->summary);
+                $content .= html_writer::link($url, $image, ['title' => $this->strings->summary]);
+                // Make sure JS file to expand course content is included.
+                $this->coursecat_include_js();
+            }
+        }
+        $content .= html_writer::end_tag('div');
+        return $content;
     }
 }
