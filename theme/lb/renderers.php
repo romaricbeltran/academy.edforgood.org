88c6835b275a42de34bf068a38fa8ccbac405022
Romaric BELTRAN <romaric.beltran@gmail.com>
2025-11-10 20:22:20 +0100

    Remove i from combolist on home page

--- theme/lb/renderers.php
+++ theme/lb/renderers.php
@@ -146,16 +146,18 @@ class theme_lb_core_course_renderer extends core_course_renderer {
         $content .= html_writer::tag($nametag, $coursenamelink, ['class' => 'coursename']);
         // If we display course in collapsed form but the course has summary or course contacts, display the link to the info page.
         $content .= html_writer::start_tag('div', ['class' => 'moreinfo']);
-        if ($chelper->get_show_courses() < self::COURSECAT_SHOW_COURSES_EXPANDED) {
-            if ($course->has_summary() || $course->has_course_contacts() || $course->has_course_overviewfiles()
-                || $course->has_custom_fields()) {
-                $url = new moodle_url('/course/info.php', ['id' => $course->id]);
-                $image = $this->output->pix_icon('i/info', $this->strings->summary);
-                $content .= html_writer::link($url, $image, ['title' => $this->strings->summary]);
-                // Make sure JS file to expand course content is included.
-                $this->coursecat_include_js();
-            }
-        }
+        # LB code start - Remove i from combolist on home page
+        //if ($chelper->get_show_courses() < self::COURSECAT_SHOW_COURSES_EXPANDED) {
+        //    if ($course->has_summary() || $course->has_course_contacts() || $course->has_course_overviewfiles()
+        //        || $course->has_custom_fields()) {
+        //        $url = new moodle_url('/course/info.php', ['id' => $course->id]);
+        //        $image = $this->output->pix_icon('i/info', $this->strings->summary);
+        //        $content .= html_writer::link($url, $image, ['title' => $this->strings->summary]);
+        //        // Make sure JS file to expand course content is included.
+        //        $this->coursecat_include_js();
+        //    }
+        //}
+        # LB code end
         $content .= html_writer::end_tag('div');
         return $content;
     }
